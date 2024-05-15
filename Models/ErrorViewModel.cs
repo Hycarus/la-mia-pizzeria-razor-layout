d7d505ccae6c1b556c85002a6cd8a607a48a4a5b
Hycarus <mirkomarasco08@gmail.com>
2024-05-15 11:44:57 +0200

    refactoring solution

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace la_mia_pizzeria_static.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
